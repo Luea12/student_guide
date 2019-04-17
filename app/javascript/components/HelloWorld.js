0e58e43c21bd54ec3f3a680abd8a4fb534b61928
Ninjseal <b.marius10@yahoo.com>
2019-04-17 11:10:36 +0300

    Application Setup (#1)
    Hello World with RoR + reactJS + MariaDB

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld
