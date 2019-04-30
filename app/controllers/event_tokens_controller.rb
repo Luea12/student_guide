bdd8d849095af94a7d12349094ea01123478d7ad
Luea12 <cristina.badic12@gmail.com>
2019-04-30 12:22:21 +0300

    EvenTokens controller added. EvenToken model added.

---
+++ app/controllers/event_tokens_controller.rb
@@ -0,0 +1,25 @@
+class EventTokensController < ApplicationController
+
+  layout 'application'
+
+  before_action :require_admin_login
+
+  def index
+    @event_tokens = EventToken.all
+    render('index')
+  end
+
+  def create
+    @event_token = EventToken.create()
+    redirect_to(event_tokens_path)
+  end
+
+  def destroy
+    if EventToken.exists?(params[:id])
+      @event_token = EventToken.find(params[:id])
+      @event_token.destroy
+    end
+    redirect_to(event_tokens_path)
+  end
+
+end
---
+++ test/models/event_token_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EventTokenTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
