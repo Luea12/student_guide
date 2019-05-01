8ded9bff07d086206c0a828d69d5aeaf706f3421
Ninjseal <b.marius10@yahoo.com>
2019-05-01 21:45:50 +0300

    Updated routes and created components
    Updated routes and added react apps for each home.

---
+++ app/javascript/components/StudentApp.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class StudentApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StudentApp
---
+++ app/javascript/components/TeacherApp.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class TeacherApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TeacherApp
