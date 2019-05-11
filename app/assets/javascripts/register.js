cea72393dd0dc79a8f0568ddafef9ffd4d9ae3b0
Tavi <tavitavi62@gmail.com>
2019-05-11 22:48:19 +0300

    Am adaugat interfata pentru Register
    Interfata merge bomba, mai avem sa adaugam un font frumos si o putem vinde

---
+++ app/assets/javascripts/register.js
@@ -0,0 +1,6 @@
+/** The function below is called when page is loaded*/
+document.addEventListener("DOMContentLoaded", function(event) {
+    const div = document.getElementById("first_name");
+    div.setAttribute('value',"");
+
+});
