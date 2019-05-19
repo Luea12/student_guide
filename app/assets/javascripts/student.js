4c476f6598ed600f468530d61fa87c8c8e0108ab
Tavi <tavitavi62@gmail.com>
2019-05-19 21:01:59 +0300

    Added AddNewEvent Form + button
    Students can add new events. This should be visible only on teachers ` page

---
+++ app/assets/javascripts/student.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
