63f4707f38efec4ebb5e3ea73ca36a883c7d2f25
Ninjseal <b.marius10@yahoo.com>
2019-07-05 20:36:40 +0300

    Updated menu and improved schedule cards interface.

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -3,18 +3,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-
-var toggled = 0;
-function toggle(){
-
-  if(toggled==0){
-    document.getElementById("arrow").style.transform = 'rotate(-180deg)';
-    document.getElementById("menu").style.left = '0';
-    toggled=1;
-  }
-  else {
-    document.getElementById("arrow").style.transform = 'rotate(0deg)';
-    document.getElementById("menu").style.left = '-100px';
-    toggled=0;
-  }
-}
