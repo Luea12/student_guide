47f5b87003757ddcabc95967f37359be2a258467
Ninjseal <b.marius10@yahoo.com>
2019-07-14 03:27:54 +0300

    Deleted unnecessary files. Cleaned and commented some teacher code.

--- app/assets/javascripts/teacher.js
+++ app/assets/javascripts/teacher.js
@@ -3,17 +3,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-});
-
-function openForm() {
-    document.getElementById("news-form").style.display = "block";
-}
-
-function closeForm() {
-    document.getElementById("news-form").style.display = "none";
-}
